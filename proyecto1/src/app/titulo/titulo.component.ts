2e00ca34a9e03f19a42a4be5571a7f487a416ffd
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-06-07 12:56:35 -0500

    Probando funciones en typescript

--- proyecto1/src/app/titulo/titulo.component.ts
+++ proyecto1/src/app/titulo/titulo.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: 'titulo.component.html' 
 })
 export class TituloComponent{
+  public name : string = 'hola';
 
+  setMessage(valor): string{
+    console.log("hola Juliana")
+    return `${valor}: el mensaje se ha usado
+          
+        Esto va a quedar de otra manera mas bonita no como la fea de juliana al lado
+    `;
+
+
+  }
+  public get nombre() : string{
+    return 'Este es el nombre Lololol';
+  }
 }
