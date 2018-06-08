186488a9c470425aa51e47f2c480908def69b2c9
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-06-08 10:03:34 -0500

    agragdo archivo externo de css, y probando las propiedades .host y :host-context(){}

--- proyecto1/src/app/titulo/titulo.component.ts
+++ proyecto1/src/app/titulo/titulo.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'titulo',
-  templateUrl: 'titulo.component.html' 
+  templateUrl: 'titulo.component.html',
+  styleUrls: ['titulo.component.css']
 })
 export class TituloComponent{
   public name : string = 'hola';
